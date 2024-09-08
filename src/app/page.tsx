8f882353df2df32e5ab0094b554c2a99ef0ef153
Naman Mani <naman.mani23@st.niituniversity.in>
2024-09-08 20:51:18 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 // Ensure this is at the top to make Home a client component
-import SimpleNavbar from "@/components/Header/header"; // Correctly import from the file
 import React from "react";
 
 const Home: React.FC = () => {
   return (
     <>
-    <SimpleNavbar/>
+    
     <h1 className="text-white">This  is the Home page</h1>
       
     </>
